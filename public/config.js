b1e0d2595b19201f013ff9cfb34dbd48ca195ae3
Sergey Kasatikov <skasatikov@team.amocrm.com>
2019-10-01 17:08:34 +0300

    codestyle fix

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ requirejs.config({
     },
     map: {
       '*': {
-        'css': 'require-css/css' // or whatever the path to require-css is
+        'css': 'require-css/css'
       }
     },
     packages: [
